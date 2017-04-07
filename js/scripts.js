d023dc5490fc1d3bcc9b56de563602ffc48f6758
nathaliazxavier <nathaliazxavier@gmail.com>
2017-04-07 21:18:05 +0100

    nova trilha + tooltips

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,10 @@ $('.casa').mouseenter(function() {
 
 	$('.casa').addClass('oculta');
 	$(this).removeClass('oculta');
-	$(targettip).fadeIn();
+	$(targettip).stop().fadeIn();
 }).mouseout( function() {
-	var targettip = $(this).attr("data-href") + "-tip";
-
 	$('.casa').removeClass('oculta');
-	$(targettip).fadeOut();
+	$('.tip').stop().fadeOut();
 }).click( function() {
 	var targetmodal = $(this).attr("data-href") + "-modal";
 	$(targetmodal).fadeIn();
