81b2614945e6cb74091df9d0df093f802df7c9b5
nathaliazxavier <nathaliazxavier@gmail.com>
2017-04-27 14:57:55 +0100

    Novo svg + animação seta + carousel automático

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-$('#instituicoes-carousel').carousel();
+$('#instituicoes-carousel').carousel({
+	interval: 5000
+});
 
 $('.casa').mouseenter(function() {
 	var targettip = $(this).attr("data-href") + "-tip";
@@ -6,13 +8,25 @@ $('.casa').mouseenter(function() {
 	$('.casa').addClass('oculta');
 	$(this).removeClass('oculta');
 	$(targettip).stop().fadeIn();
+
 }).mouseout( function() {
 	$('.casa').removeClass('oculta');
 	$('.tip').stop().fadeOut();
+
 }).click( function() {
 	var targetmodal = $(this).attr("data-href") + "-modal";
 	$(targetmodal).fadeIn();
 });
+
+
+
+$('.casa:last-child').mouseenter(function() {
+	$('.seta').stop().show();
+}).mouseout( function() {
+	$('.seta').stop().hide();
+});
+
+
 
 $('.modal').click( function(e) {
 	if ($(e.target).hasClass("modal")) {
